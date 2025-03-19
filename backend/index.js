c7784781f59140479b771ccffb0059d403f7a23d
AterixGz <128670686+AterixGz@users.noreply.github.com>
2025-03-19 20:40:49 +0700

    Create Backend

---
+++ backend/index.js
@@ -0,0 +1,8 @@
+//สร้างไฟล์ Server พื้นฐานของ Express
+const express = require('express');
+const app = express();
+const port = 3000;
+
+app.listen(port , () => {
+    console.log(`Server is running on port ${port}`);
+});
